f32d8af3488a9bdc005ac1952eb039c1c0b96372
Raffaele Di Martino <raf@raffaeles-mbp.home>
2023-01-28 16:13:54 +0000

    Testing API data and ajax

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,18 @@
 
 
+const API = "7064144a9fb66ec04495c5ff9bb9d495";
+const weatherToday = $("#today");
+const weatherForecast = $("#forecast");
+
+
+//ajax call
+function getCity(){
+    var queryURL ="https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + API;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function(data) {
+            console.log(data)
+
+    });
+}
