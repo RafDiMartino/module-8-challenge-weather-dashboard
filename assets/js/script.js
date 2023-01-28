61eff7aa4247b6ae54f199612f106070ba7b2e77
Raffaele Di Martino <raf@raffaeles-mbp.home>
2023-01-28 16:14:57 +0000

    adding event listener to the search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,15 @@
 const API = "7064144a9fb66ec04495c5ff9bb9d495";
 const weatherToday = $("#today");
 const weatherForecast = $("#forecast");
+var cityName = ""
 
+// function for the click event
+$("#search-button").click(function(e) {
+    e.preventDefault()
+    cityName = $("#search-input").val();
+    console.log(cityName)
+    getCity()
+});
 
 //ajax call
 function getCity(){
