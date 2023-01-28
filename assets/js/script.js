35800607edb3c6f21b44e6e838682876ea64daba
Raffaele Di Martino <raf@raffaeles-mbp.home>
2023-01-28 16:17:19 +0000

    fetching and displaying the current weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,13 +14,28 @@ $("#search-button").click(function(e) {
 });
 
 //ajax call
-function getCity(){
+function getCurrentCity(){
     var queryURL ="https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + API;
     $.ajax({
         url: queryURL,
         method: "GET"
         }).then(function(data) {
             console.log(data)
-
+            var city = data.city.name
+            var todayDate = moment().format('ddd Do MMMM YYYY')
+            var weatherIcon ="http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png";
+            var weatherIconAlt = data.list[0].weather[0].main
+            var temperature = data.list[0].main.temp - 273.15
+            var windSpeed = data.list[0].wind.speed
+            var humidity = data.list[0].main.humidity
+            
+            weatherToday.html(`
+                <h2>${city}</h2>
+                <h2>${todayDate}</h2>
+                <img src="${weatherIcon}" alt="${weatherIconAlt}">
+                <p>Temp: ${temperature.toFixed(2)} °C</p>
+                <p>Wind: ${windSpeed} m/s</p>
+                <p>Humidity: ${humidity}%</p>
+            `)
     });
 }
